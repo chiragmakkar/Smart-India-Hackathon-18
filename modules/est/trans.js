0222d40ec33fe1b7d6e53c244a87b77167eca236
Aaditya Chakravarty <ac36500@gmail.com>
2018-03-30 23:25:31 +0530

    Trans Century

--- modules/est/trans.js
+++ modules/est/trans.js
@@ -10,8 +10,8 @@ const getEstimation = (req,res) => {
     let co = geo(req.body.address)
     co.then((out) => {
         let co = out.results[0].geometry.location
-        let req_capacity = 300
         let allowedNodes = []
+        var ec = 0
 
         nodes.find({}, (err,data) => {
           if(err) {
@@ -19,45 +19,61 @@ const getEstimation = (req,res) => {
           }
           else {
             data.forEach((e) => {
-              if(rad(co.lat,co.lng,e.location.lattitude,e.location.longitude) <= 10000) {
-                if((e.maxCapacity - e.currentCapacity) > req_capacity) {
+              if(rad(co.lat,co.lng,e.location.lattitude,e.location.longitude) <= 100) {
+                if(e.currentCapacity <= (70/100)*e.maxCapacity) {
                   allowedNodes.push(e)
                 }
+                else {
+                  ec = 1
+                  return;
+                }
+              }
+              else {
+                ec = 2
+                return;
               }
             })
 
-            var maxNode = allowedNodes[0]
+            var minNode = allowedNodes[0]
+            var finalEst
 
-            var initDist = rad(co.lat,co.lng,maxNode.location.lattitude,maxNode.location.longitude)
+            var initDist = rad(co.lat,co.lng,minNode.location.lattitude,minNode.location.longitude)
+            var initEst = 2500+(minNode.rate*initDist*1000)
 
-            var estimate = 2500+(maxNode.rate*initDist*1000);
+            allowedNodes.forEach((elem) => {
 
-            allowedNodes.forEach((e) => {
+              var currDist = rad(co.lat,co.lng,elem.location.lattitude,elem.location.longitude)
+              var currEst = 2500+(elem.rate*currDist*1000)
 
-              let dist = rad(co.lat,co.lng,e.location.lattitude,e.location.longitude)
-              let currEstimate = 2500+(e.rate*dist*1000);
-
-              if(currEstimate < estimate) {
-                maxNode = e
-                estimate = dist*rate
+              if(currEst <= initEst) {
+                minNode = elem
+                finalEst = currEst
               }
+
             })
-            let final = {
+
+            var final = {
               "coordinates":{
                 "transformer":{
-                  "lat":maxNode.location.lattitude,
-                  "lng":maxNode.location.longitude
+                  "lat":minNode.location.lattitude,
+                  "lng":minNode.location.longitude
                 },
                 "customer":{
                   "lat":co.lat,
                   "lng":co.lng
                 }
               },
-              "estimatedCost":parseInt(estimate),
-              "requiredQuantity":req_capacity,
-              "provider":maxNode.provider
+              "estimatedCost":parseInt(finalEst),
+              "freeCapacity":minNode.maxCapacity-minNode.currentCapacity,
+              "provider":minNode.provider
             }
-            res.json(final)
+            if(!allowedNodes) {
+                res.json({"error":"No nodes found within 1KM.","code":ec})
+            }
+            else {
+                res.json(final)
+            }
+
           }
         })
     }, (err) => {
