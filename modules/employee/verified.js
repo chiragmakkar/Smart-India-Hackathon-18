7404bd6d49a5ce0baa074d707a893fb1e9d299fc
chiragmakkar <chirag.piet@gmail.com>
2018-03-31 14:35:15 +0530

    verifinance true

--- modules/employee/verified.js
+++ modules/employee/verified.js
@@ -3,9 +3,24 @@ const mongoose = require('mongoose')
 const Consumer = require(__base + 'models/consumer.js').consumer
 
 const verified = (req, res) => {
-    Consumer.findOneAndUpdate({"ApplicationID":req.body.applicationId}, {
-        $set:{
-            "applicationTracking.veriFinance":true
+    Consumer.findOneAndUpdate({ "ApplicationID": req.body.applicationId }, {
+        $set: {
+            "applicationTracking":
+                {
+                    "veriFinance": true
+                }
+        }, 
+    }, (err, data) => {
+        if (err) console.log(err)
+
+        else{
+            Consumer.findOne({ "ApplicationID": req.body.applicationId }, (err, data) => {
+                if (err) console.log(err)
+        
+                else {
+                    res.send(data)
+                }
+            })
         }
     })
 }
